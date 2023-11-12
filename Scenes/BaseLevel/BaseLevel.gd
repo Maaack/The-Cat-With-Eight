483fbe805ff2870758de43e83be05edbf37bfbd4
Maaack <marek.belski@gmail.com>
2023-11-11 20:42:04 -0800

    Smooth camera and add different level dialogues.

--- Scenes/BaseLevel/BaseLevel.gd
+++ Scenes/BaseLevel/BaseLevel.gd
@@ -1,11 +1,14 @@
 extends Node2D
+class_name BaseLevel
 
 signal level_ended
 signal dialogue_started(title : String)
 
+@export var opening_dialogue_title : String
+
 func _ready():
 	await(get_tree().create_timer(0.1).timeout)
-	start_dialogue("Story_1_1")
+	start_dialogue(opening_dialogue_title)
 
 func end_level():
 	emit_signal("level_ended")
@@ -13,7 +16,6 @@ func end_level():
 func start_dialogue(dialogue_title : String):
 	emit_signal("dialogue_started", dialogue_title)
 
-
 func _on_exit_area_2d_body_entered(body):
 	if body.is_in_group(Constants.TIGER_GROUP):
-		emit_signal("level_ended")
+		end_level()
