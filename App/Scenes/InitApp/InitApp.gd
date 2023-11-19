71cf1462dd091d0e64054f8789f9ec2571f7ac90
Maaack <marek.belski@gmail.com>
2023-11-19 09:04:15 -0800

    Init app will skip opening if seen.

--- App/Scenes/InitApp/InitApp.gd
+++ App/Scenes/InitApp/InitApp.gd
@@ -1,8 +1,12 @@
 extends Node
 
-@export_file("*.tscn") var next_scene : String
+@export_file("*.tscn") var opening_scene : String
+@export_file("*.tscn") var main_menu_scene : String
 
 func _ready():
 	AppLog.app_opened()
 	AppSettings.set_from_config_and_window(get_window())
-	SceneLoader.load_scene(next_scene)
+	if GameLog.has_seen_opening():
+		SceneLoader.load_scene(main_menu_scene)
+	else:
+		SceneLoader.load_scene(opening_scene)
